e6d65f7ad8ae12cfdaa7d7e88025e9de7692518c
vmdharan <rexar7@gmail.com>
2016-12-15 18:08:25 +1100

    Handle multiple test cases
    - Handle multiple test cases in the test directory.
    - Check and process test cases with the '.tst' extension.

--- src/RunTests.py
+++ src/RunTests.py
@@ -1,28 +1,48 @@
 # RunTests.py
 # Run the Fingerprint program on the test cases.
 
-import Fingerprint
+import os
 import csv
+import Fingerprint
 
+# Instantiate the Fingerprint class.
 f = Fingerprint.Fingerprint()
 
-# Open the test file
-with open('../tests/testfile1.csv', 'r') as tf:
-    reader = csv.reader(tf)
-    fp_data = list(reader)
+# Loop over all the test cases in the test directory.
+test_directory = '../tests'
+for test_file in os.listdir(test_directory):
+    if (test_file.endswith('.tst')):
+        print (test_file)
 
-# Process the input and identify the grouping.
-print(fp_data[0])
-print(fp_data[1])
+        # Evaluate the full path to the file.
+        test_file_path = test_directory + '/' + test_file
+        print(test_file_path)
+        
+        # Open the test file
+        with open(test_file_path, 'r') as tf:
+            reader = csv.reader(tf)
+            fp_data = list(reader)
 
-exp_result = [int(x) for x in fp_data[1]]
+            # Process the input and identify the grouping.
+            print(fp_data[0])
+            print(fp_data[1])
 
-test_result = f.Henry(fp_data[0])
-print(test_result)
-print(exp_result)
+            # Get the expected result.
+            exp_result = [int(x) for x in fp_data[1]]
 
-# Determine whether the application passes or fails testing.
-if (test_result != exp_result):
-    print('Test failed.')
-else:
-    print('Test passed.')
+            # Get the actual test result.
+            test_result = f.Henry(fp_data[0])
+
+            # Display results.
+            print(test_result)
+            print(exp_result)
+
+            # Determine whether the application passes or fails testing.
+            if (test_result != exp_result):
+                print('Test failed.')
+            else:
+                print('Test passed.')
+
+    # Display an error if the test case could not be accessed.
+    else:
+        print ('Unspecified error')
