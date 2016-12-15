44e890f057c840e13caf73e41c8f3c1e9c0b420a
vmdharan <rexar7@gmail.com>
2016-12-15 18:46:34 +1100

    Added test cases
    - Added test cases (13 new cases, 2 existing cases removed, 1 new case
    added to test failure).
    - Changed test case file extension format to .dat.

--- src/RunTests.py
+++ src/RunTests.py
@@ -11,12 +11,12 @@ f = Fingerprint.Fingerprint()
 # Loop over all the test cases in the test directory.
 test_directory = '../tests'
 for test_file in os.listdir(test_directory):
-    if (test_file.endswith('.tst')):
-        print (test_file)
+    if (test_file.endswith('.dat')):
+        #print (test_file)
 
         # Evaluate the full path to the file.
         test_file_path = test_directory + '/' + test_file
-        print(test_file_path)
+        #print(test_file_path)
         
         # Open the test file
         with open(test_file_path, 'r') as tf:
@@ -24,8 +24,8 @@ for test_file in os.listdir(test_directory):
             fp_data = list(reader)
 
             # Process the input and identify the grouping.
-            print(fp_data[0])
-            print(fp_data[1])
+            #print(fp_data[0])
+            #print(fp_data[1])
 
             # Get the expected result.
             exp_result = [int(x) for x in fp_data[1]]
@@ -34,14 +34,18 @@ for test_file in os.listdir(test_directory):
             test_result = f.Henry(fp_data[0])
 
             # Display results.
-            print(test_result)
-            print(exp_result)
+            #print(test_result)
+            #print(exp_result)
 
             # Determine whether the application passes or fails testing.
             if (test_result != exp_result):
-                print('Test failed.')
+                print(test_file + ' - ' + 'Test failed.')
+                print('Expected result: ')
+                print (exp_result)
+                print('Actual result: ')
+                print (test_result)
             else:
-                print('Test passed.')
+                print(test_file + ' - ' + 'Test passed.')
 
     # Display an error if the test case could not be accessed.
     else:
