8daf127b2ecd61d1e9bfc68715897b4ab9757f7f
vmdharan <rexar7@gmail.com>
2016-12-15 17:21:58 +1100

    Start work on a testing framework
    - Create a script to run testing.
    - Add an initial test case.

---
+++ src/RunTests.py
@@ -0,0 +1,27 @@
+# RunTests.py
+# Run the Fingerprint program on the test cases.
+
+import Fingerprint
+import csv
+
+f = Fingerprint.Fingerprint()
+
+# Open the test file
+    reader = csv.reader(tf)
+    fp_data = list(reader)
+
+# Process the input and identify the grouping.
+print(fp_data[0])
+print(fp_data[1])
+
+exp_result = [int(x) for x in fp_data[1]]
+
+test_result = f.Henry(fp_data[0])
+print(test_result)
+print(exp_result)
+
+# Determine whether the application passes or fails testing.
+if (test_result != exp_result):
+    print('Test failed.')
+else:
+    print('Test passed.')
